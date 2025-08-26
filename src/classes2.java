df40d08a90cccbef059de429cbd7e796ffe429c6
alexey.astafyev <astafev.aa@gmail.com>
2025-08-26 16:11:39 +0400

    На вход подается строка, которая содержит целое число. Необходимо его принять через Scanner, далее его преобразовать строку в Integer, прибавить 30 и присвоить результат переменной result.

---
+++ src/classes2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class classes2 {
+    public static void main(String[] args) {
+
+        Integer val1 = 898;
+        Integer val2 = 898;
+
+        Boolean result1 = val1 == val2;
+        Boolean result = val1.equals(val2);
+
+        System.out.println(result1);
+        System.out.println(result);
+    }
+}
